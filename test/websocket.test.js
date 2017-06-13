e09e2fd6c065eab9cd0a96f0499a46df36b3e82b
Lukas Rezek <lukas@miratronix.com>
2017-06-12 21:39:11 -0400

    Add onProgressUpdate for rapport-http

--- test/websocket.test.js
+++ test/websocket.test.js
@@ -2,15 +2,11 @@
 
 const Rapport = require('rapport');
 const RapportRouter = require('rapport-router');
-const manualWrap = require('rapport/lib/websocket/index.js');
-const manualStandardize = require('rapport/lib/standardize.js');
-const manualRequestCache = require('rapport/lib/request.cache.js');
-const manualOptions = require('rapport/lib/options.js');
+const RapportHttp = require('rapport-http');
 
 const util = require('./index.js');
 const plugin = require('../lib/index.js');
 const handlerCache = require('../lib/handler.cache.js');
-const extendRequest = require('../lib/websocket/request.js');
 
 describe('Websocket', () => {
 
@@ -87,57 +83,6 @@ describe('Websocket', () => {
             });
         });
 
-        it('Adds a real sendProgressUpdate to the rapport-router responder object when the message is a request', () => {
-            rapport.use(RapportRouter);
-            rapport.use(plugin);
-
-            return new Promise((resolve) => {
-                testSocket = rapport.wrap(mockSocket, {
-                    router: {
-                        handle: (req, res) => {
-                            res.should.have.a.property('sendProgressUpdate').that.is.a('function');
-                            res.sendProgressUpdate('Progress!');
-                            const message = JSON.parse(mockSocket.lastSentMessage);
-                            message.should.have.a.property('_pu');
-                            message.should.have.a.property('_b').that.equals('Progress!');
-                            resolve();
-                        }
-                    }
-                });
-                mockSocket.fire('message', JSON.stringify({
-                    _rq: 'requestId',
-                    _b: {
-                        _u: 'url',
-                        _m: 'method',
-                        _b: 'body'
-                    }
-                }));
-            });
-        });
-
-        it('Adds a no-op sendProgressUpdate to the rapport-router responder object when the message is not a request', () => {
-            rapport.use(RapportRouter);
-            rapport.use(plugin);
-
-            return new Promise((resolve) => {
-                testSocket = rapport.wrap(mockSocket, {
-                    router: {
-                        handle: (req, res) => {
-                            res.should.have.a.property('sendProgressUpdate').that.is.a('function');
-                            res.sendProgressUpdate('Progress!');
-                            mockSocket.lastSentMessage.should.equal('');
-                            resolve();
-                        }
-                    }
-                });
-                mockSocket.fire('message', JSON.stringify({
-                    _u: 'url',
-                    _m: 'method',
-                    _b: 'body'
-                }));
-            });
-        });
-
         it('Can send a progress update using the responder object', () => {
             return new Promise((resolve) => {
                 testSocket.onMessage((msg, ws) => {
@@ -192,14 +137,8 @@ describe('Websocket', () => {
         });
 
         it('Throws an error if a callback is not supplied and there is no Promise object', () => {
-            const options = manualOptions();
-            const requestCache = manualRequestCache();
-            const standardSocket = manualStandardize(mockSocket);
-            const testSocket = manualWrap(standardSocket, requestCache, options);
+            const testSocket = rapport.wrap(mockSocket, { Promise: false });
 
-            delete options.Promise;
-
-            extendRequest(testSocket, requestCache, options);
             (() => {
                 testSocket.request('Something');
             }).should.throw(Error, 'Can\'t make a request without a Promise implementation or callback');
@@ -214,6 +153,204 @@ describe('Websocket', () => {
                 _pu: 'someId',
                 _b: { hello: 'world' }
             }));
+        });
+    });
+
+    context('rapport-router', () => {
+
+        it('Adds a real sendProgressUpdate to the responder object when the message is a request', () => {
+            return new Promise((resolve) => {
+                testSocket = Rapport().use(RapportRouter).use(plugin).wrap(mockSocket, {
+                    router: {
+                        handle: (req, res) => {
+                            res.should.have.a.property('sendProgressUpdate').that.is.a('function');
+                            res.sendProgressUpdate('Progress!');
+                            const message = JSON.parse(mockSocket.lastSentMessage);
+                            message.should.have.a.property('_pu');
+                            message.should.have.a.property('_b').that.equals('Progress!');
+                            resolve();
+                        }
+                    }
+                });
+                mockSocket.fire('message', JSON.stringify({
+                    _rq: 'requestId',
+                    _b: {
+                        _u: 'url',
+                        _m: 'method',
+                        _b: 'body'
+                    }
+                }));
+            });
+        });
+
+        it('Adds a no-op sendProgressUpdate to the responder object when the message is not a request', () => {
+            return new Promise((resolve) => {
+                testSocket = Rapport().use(RapportRouter).use(plugin).wrap(mockSocket, {
+                    router: {
+                        handle: (req, res) => {
+                            res.should.have.a.property('sendProgressUpdate').that.is.a('function');
+                            res.sendProgressUpdate('Progress!');
+                            mockSocket.lastSentMessage.should.equal('');
+                            resolve();
+                        }
+                    }
+                });
+                mockSocket.fire('message', JSON.stringify({
+                    _u: 'url',
+                    _m: 'method',
+                    _b: 'body'
+                }));
+            });
+        });
+    });
+
+    context('rapport-http', () => {
+
+        it('Adds a onProgressUpdate function', () => {
+            testSocket = Rapport().use(RapportHttp).use(plugin).wrap(mockSocket);
+            testSocket.http('put', '/test').should.have.a.property('onProgressUpdate').that.is.a('function');
+        });
+
+        it('Adds a onProgressUpdate function to get', () => {
+            testSocket = Rapport().use(RapportHttp).use(plugin).wrap(mockSocket);
+            testSocket.get('/test').should.have.a.property('onProgressUpdate').that.is.a('function');
+        });
+
+        it('Adds a onProgressUpdate function to post', () => {
+            testSocket = Rapport().use(RapportHttp).use(plugin).wrap(mockSocket);
+            testSocket.post('/test').should.have.a.property('onProgressUpdate').that.is.a('function');
+        });
+
+        it('Adds a onProgressUpdate function to put', () => {
+            testSocket = Rapport().use(RapportHttp).use(plugin).wrap(mockSocket);
+            testSocket.put('/test').should.have.a.property('onProgressUpdate').that.is.a('function');
+        });
+
+        it('Adds a onProgressUpdate function to delete', () => {
+            testSocket = Rapport().use(RapportHttp).use(plugin).wrap(mockSocket);
+            testSocket.delete('/test').should.have.a.property('onProgressUpdate').that.is.a('function');
+        });
+
+        it('Adds a onProgressUpdate function to patch', () => {
+            testSocket = Rapport().use(RapportHttp).use(plugin).wrap(mockSocket);
+            testSocket.patch('/test').should.have.a.property('onProgressUpdate').that.is.a('function');
+        });
+
+        it('Calls the progress update handler when a progress update is received using promises', () => {
+            testSocket = Rapport().use(RapportHttp).use(plugin).wrap(mockSocket);
+
+            return new Promise((resolve, reject) => {
+                testSocket.get('/test')
+                    .onProgressUpdate(resolve)
+                    .send()
+                    .then((reject));
+                const message = JSON.parse(mockSocket.lastSentMessage);
+                mockSocket.fire('message', JSON.stringify({
+                    _pu: message._rq,
+                    _b: 'Hello'
+                }));
+            });
+        });
+
+        it('Calls the progress update handler when a progress update is received using callbacks', () => {
+            testSocket = Rapport().use(RapportHttp).use(plugin).wrap(mockSocket);
+
+            return new Promise((resolve, reject) => {
+                testSocket.get('/test')
+                    .onProgressUpdate(resolve)
+                    .send(reject);
+                const message = JSON.parse(mockSocket.lastSentMessage);
+                mockSocket.fire('message', JSON.stringify({
+                    _pu: message._rq,
+                    _b: 'Hello'
+                }));
+            });
+        });
+
+        it('Still sends the request object alone when no response is expected', () => {
+            testSocket = Rapport().use(RapportHttp).use(plugin).wrap(mockSocket);
+
+            testSocket.http('get', '/test').body('hello').expectResponse(false).send();
+            const lastSentMessage = JSON.parse(mockSocket.lastSentMessage);
+            lastSentMessage.should.have.a.property('_u').that.equals('/test');
+            lastSentMessage.should.have.a.property('_m').that.equals('get');
+            lastSentMessage.should.have.a.property('_b').that.equals('hello');
+        });
+
+        it('Still translates a good response when using a callback', () => {
+            testSocket = Rapport().use(RapportHttp).use(plugin).wrap(mockSocket, { Promise: false });
+
+            return new Promise((resolve) => {
+                testSocket.http('get', '/test').send((res) => {
+                    res.should.have.a.property('status').that.equals(42);
+                    res.should.have.a.property('body').that.equals('hello');
+                    resolve();
+                });
+                mockSocket.fire('message', {
+                    _rs: JSON.parse(mockSocket.lastSentMessage)._rq,
+                    _b: {
+                        _s: 42,
+                        _b: 'hello'
+                    }
+                });
+            });
+        });
+
+        it('Still translates an error response when using a callback', () => {
+            testSocket = Rapport().use(RapportHttp).use(plugin).wrap(mockSocket, { Promise: false });
+
+            return new Promise((resolve) => {
+                testSocket.http('get', '/test').send((res, err) => {
+                    err.should.have.a.property('status').that.equals(42);
+                    err.should.have.a.property('body').that.equals('goodbye');
+                    resolve();
+                });
+                mockSocket.fire('message', {
+                    _rs: JSON.parse(mockSocket.lastSentMessage)._rq,
+                    _e: {
+                        _s: 42,
+                        _b: 'goodbye'
+                    }
+                });
+            });
+        });
+
+        it('Still translates a good response when using a promise', () => {
+            testSocket = Rapport().use(RapportHttp).use(plugin).wrap(mockSocket);
+
+            return new Promise((resolve) => {
+                testSocket.http('get', '/test').send().then((res) => {
+                    res.should.have.a.property('status').that.equals(42);
+                    res.should.have.a.property('body').that.equals('hello');
+                    resolve();
+                });
+                mockSocket.fire('message', {
+                    _rs: JSON.parse(mockSocket.lastSentMessage)._rq,
+                    _b: {
+                        _s: 42,
+                        _b: 'hello'
+                    }
+                });
+            });
+        });
+
+        it('Still translates an error response when using a promise', () => {
+            testSocket = Rapport().use(RapportHttp).use(plugin).wrap(mockSocket);
+
+            return new Promise((resolve) => {
+                testSocket.http('get', '/test').send().catch((err) => {
+                    err.should.have.a.property('status').that.equals(42);
+                    err.should.have.a.property('body').that.equals('goodbye');
+                    resolve();
+                });
+                mockSocket.fire('message', {
+                    _rs: JSON.parse(mockSocket.lastSentMessage)._rq,
+                    _e: {
+                        _s: 42,
+                        _b: 'goodbye'
+                    }
+                });
+            });
         });
     });
 });
